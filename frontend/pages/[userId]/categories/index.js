45ade772e7cf1afd0c75338056a5d820a2fd8ef7
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-01-26 13:40:19 +0200

    first backend add

--- frontend/pages/[userId]/categories/index.js
+++ frontend/pages/[userId]/categories/index.js
@@ -6,7 +6,7 @@ import Nav from './nav';
 import ItemsContainer from './items';
 import TheItemContainer from './theItem';
 
-const Home = ({ appData , userId}) => {
+const Home = ({ appData , userId, changeSearchPage}) => {
     const allItems = appData?.items
     const theCategories = appData?.categories
     const [selectedItem, setSelectedItem] = useState(null)
@@ -36,13 +36,13 @@ const Home = ({ appData , userId}) => {
                                 <TheItemContainer selectedItem={selectedItem} setSelectedItem={setSelectedItem}/>
                                 <p>Similar items</p>
                                 <ItemsContainer theItemsData={reduceItems} setSelectedItem={setSelectedItem}
-                                    selectedItem={selectedItem}
+                                    selectedItem={selectedItem} changeSearchPage={changeSearchPage}
                                 />
                             </>
                         ):(
                             <>
                                 <ItemsContainer theItemsData={categoryItems} setSelectedItem={setSelectedItem}
-                                    selectedItem={selectedItem}
+                                    selectedItem={selectedItem} changeSearchPage={changeSearchPage}
                                 />
                                 <div>
                                     <div className={componentStyles.pageNav}>
