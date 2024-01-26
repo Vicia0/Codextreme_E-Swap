4610c9d05a1af0f169cea7b032798ed9956a17ad
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-01-26 17:49:45 +0200

    DashHead top edits

--- frontend/pages/[userId]/dashboard/dashHead.js
+++ frontend/pages/[userId]/dashboard/dashHead.js
@@ -12,43 +12,51 @@ export const DashHead = ({changeSearchPage}) => {
   useEffect(() => {
     const interval = setInterval(() => {
       changeIndex();
-    }, 1000);
+    }, 3000);
 
     return () => clearInterval(interval);
   }, []);
 
   const contents = {
     newly_added: { heading: 'Newly Added', subheading: 'Check items >>' },
-    most_bought_category: { heading: 'Most Bought Category', subheading: 'Check items >>' },
-    eco_swap_choosing: { heading: 'Eco Swap Choosing', subheading: 'Check items >>' },
+    most_bought_category: { heading: 'Most Bought Category', subheading: 'Check items >>'},
+    eco_swap_choosing: { heading: 'Eco Swap Choosing', subheading: 'Check items >>'},
   };
   
   return (
     <PageHeadLayout>
       <div id={styles.DashHead}>
         <div className={styles.searchHolder}>
-          <input id={styles.searchBar} type="text" placeholder="Q mouse" onClick={()=>changeSearchPage(false)}/>
-          <div id={styles.message} /> {/* Corrected the typo here */}
+          <input id={styles.searchBar} type="text" placeholder="Q mouse" onClick={() => changeSearchPage(false)} />
+          <div id={styles.message} /> 
         </div>
         <div className={styles.contentsWrapper} onMouseEnter={changeIndex}>
-          {Object.entries(contents).map(([key, content], index) => (
-            <div key={key} className={`${styles.contentsContainer} ${index === currentIndex ? styles.current : ''}`}>
-              <div className={styles.content}>
+          {Object.entries(contents).map(([key, content], index) => {
+            console.log(key,': ', content.image)
+            return(
+              <div key={key}
+                className={`${styles.contentsContainer} ${index === currentIndex ? styles.current : ''}`}
+              >
+                <div className={styles.content}>
                   <h2>{content.heading}</h2>
                   <p>{content.subheading}</p>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-        <div className={styles.circlesWrapper}>
+            )}
+          )}
+
+          <div className={styles.circlesWrapper}>
           {Object.keys(contents).map((key, index) => (
             <div
               key={key}
               className={`${styles.circle} ${index === currentIndex ? styles.currentCircle : ''}`}
             />
           ))}
+          </div>
         </div>
+
       </div>
     </PageHeadLayout>
   );
+  
 };
