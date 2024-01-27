e742cf3566278b5b199a4330bb3da1d70a68887c
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-01-27 10:32:40 +0200

    using dummy data

--- frontend/pages/[userId]/dashboard/seller/row1.js
+++ frontend/pages/[userId]/dashboard/seller/row1.js
@@ -1,34 +1,28 @@
 // Row1.js
 import styles from '../../../../styles/module.css/allPages/Dash_Content.module.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Image from 'next/image';
-import HomeImage from '../../../../assets/images/categories/home.png';
-import PhonesImage from '../../../../assets/images/categories/phones.png';
-import DesktopImage from '../../../../assets/images/categories/desktop.png';
-import OtherImage from '../../../../assets/images/categories/other.jpg';
+
 const Row1 = ({row1Data}) => {
+
     const the_items = row1Data
-    const category_images = {
-        'Home': HomeImage,
-        'phones': PhonesImage,
-        'desktop': DesktopImage,
-    }    
     const returnCard = (key, dict) =>{
         return(
-            <div className={styles.card} key={key}>
-                <div className={styles.top}>
-                    <Image src={category_images[dict.image] ? category_images[dict.image] : OtherImage} alt={dict.name} width={40} height={40}/>
-                </div>
-                <div className={styles.bottom}>
+            <div className={styles.card}>
+                <article >
+                    <Image src={dict.image} alt={dict.name} width={40} height={40}/>
+                </article>
+                <aside>
                     <p>{dict.name}</p>
-                </div>
+                    <p>{dict.amount} rwf</p>
+                </aside>
             </div>
         )
     }
     return (
-        <div className={styles.row1}>
+        <div className={styles.row2}>
             {
-                the_items &&
+                the_items && 
                 Object.keys(the_items).map(key=>(
                     returnCard(key, the_items[key])
                 ))
